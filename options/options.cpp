72afed96717f36ef9ad0dabe2a79aa6a466652b5
Przemyslaw Bądaruk <przemekkth@o2.pl>
2018-03-07 20:23:24 +0100

    test

---
+++ options/options.cpp
@@ -0,0 +1,129 @@
+#include <QPushButton>
+#include <QLabel>
+#include <QComboBox>
+#include <QVBoxLayout>
+#include <QTranslator>
+#include "options.h"
+#include <QEvent>
+#include <QMessageBox>
+#include <QFile>
+extern QTranslator *qTranslator;
+
+Options::Options(QWidget *parent) : QWidget(parent)
+{
+	createWidgets();
+	createLayout();
+	translateUi();
+	createConnections();
+	createStyleSheet();
+	qTranslator->load("C:\\Users\\Przemysław\\Desktop\\Gry\\Options\\en_EN.qm");
+}
+
+void Options::createWidgets()
+{
+	
+	
+	m_TitleLabel = new QLabel();
+	m_LanguagesLabel = new QLabel();
+	m_WLabel = new QLabel();
+	m_DLabel = new QLabel();
+	m_SLabel = new QLabel();
+	m_ALabel = new QLabel();
+	m_PLabel = new QLabel();
+	m_SpaceLabel = new QLabel();
+	
+	m_BackButton = new QPushButton();
+	m_ChangeLangComboBox = new QComboBox();
+	for(int i = 0 ; i < 4; i++)
+	{
+		m_ChangeLangComboBox->addItem("");
+	}
+}
+
+void Options::createLayout()
+{
+	resize(306, 300);
+	QVBoxLayout *layout = new QVBoxLayout(this);
+	layout->addWidget(m_TitleLabel);
+	layout->addWidget(m_LanguagesLabel);
+	layout->addWidget(m_ChangeLangComboBox);
+	layout->addWidget(m_WLabel);
+	layout->addWidget(m_DLabel);
+	layout->addWidget(m_SLabel);
+	layout->addWidget(m_ALabel);
+	layout->addWidget(m_PLabel);
+	layout->addWidget(m_SpaceLabel);
+	layout->addWidget(m_BackButton);	
+	setLayout(layout);
+}
+
+void Options::translateUi()
+{
+	m_TitleLabel->setText( tr("Game Options") );
+	m_LanguagesLabel->setText( tr("Languages:") );
+	
+	
+    m_ChangeLangComboBox->setItemText(0, tr("English") );
+    m_ChangeLangComboBox->setItemText(1, tr("Polski") );
+    m_ChangeLangComboBox->setItemText(2, tr("Deutsch") );
+    m_ChangeLangComboBox->setItemText(3, tr("русский") );
+	
+	m_WLabel->setText( tr("1.W - up") );
+	m_DLabel->setText( tr("2.D - right") );
+	m_SLabel->setText( tr("3.S - down") );
+	m_ALabel->setText( tr("4.A - left") );
+	m_PLabel->setText( tr("5.P - pause") );
+	m_SpaceLabel->setText( tr("6.Space - start") );
+	
+	m_BackButton->setText( tr("Back") );
+}
+
+void Options::changeEvent(QEvent *e)
+{
+
+    if(e->type() == QEvent::LanguageChange )
+    {
+        translateUi();
+    }
+    else
+    {
+        QWidget::changeEvent(e);
+    }
+}
+
+void Options::createConnections()
+{
+	connect(m_ChangeLangComboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(setCurrentLanguage(int)));
+}
+
+void Options::setCurrentLanguage(int choice_)
+{
+	
+	    switch (choice_) {
+    case 0:
+        qTranslator->load("C:\\Users\\Przemysław\\Desktop\\Gry\\Options\\en_EN.qm");
+		break;
+    case 1:
+        qTranslator->load("C:\\Users\\Przemysław\\Desktop\\Gry\\Options\\pl_PL.qm");
+        break;
+    case 2:
+        qTranslator->load("C:\\Users\\Przemysław\\Desktop\\Gry\\Options\\ger_GER");
+        break;
+    case 3:
+		qTranslator->load("C:\\Users\\Przemysław\\Desktop\\Gry\\Options\\rus_RUS");
+		break;
+    }
+}
+
+void Options::createStyleSheet()
+{
+	setStyleSheet("QPushButton{background-color: #13100a; border-style: solid;  border-width: 1px; border-radius: 10px; color: white; font-size: 20px; font-family: 'Arial'; font-weight: 500px;} QPushButton:hover{background-color: #24211b;}");
+	m_TitleLabel->setStyleSheet("font-size: 40px; font-weight: 800");
+	m_ChangeLangComboBox->setStyleSheet("background-color: black; color: red; font: italic 12pt \"Cambria\" ");
+	m_WLabel->setStyleSheet("font: italic 12pt \"Cambria\" ");
+	m_DLabel->setStyleSheet("font: italic 12pt \"Cambria\" ");
+	m_SLabel->setStyleSheet("font: italic 12pt \"Cambria\" ");
+	m_ALabel->setStyleSheet("font: italic 12pt \"Cambria\" ");
+	m_PLabel->setStyleSheet("font: italic 12pt \"Cambria\" ");
+	m_SpaceLabel->setStyleSheet("font: italic 12pt \"Cambria\" ");
+}
---
+++ options/options.h
@@ -0,0 +1,31 @@
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+#include <QWidget>
+class QLabel;
+class QPushButton;
+class QComboBox;
+
+class Options : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Options(QWidget *parent = 0);
+private:
+	void createWidgets();
+	void createLayout();
+	void createConnections();
+	void createStyleSheet();
+	void translateUi();
+	QLabel *m_TitleLabel, *m_LanguagesLabel, *m_WLabel, *m_DLabel,
+			*m_SLabel, *m_ALabel, *m_PLabel, *m_SpaceLabel;
+	QPushButton *m_BackButton;
+	QComboBox *m_ChangeLangComboBox;
+protected:
+    void changeEvent(QEvent*);
+private slots:
+	
+	void setCurrentLanguage(int);
+};
+
+#endif // THEME_H
